21230023cb115a94f28c61187bc9666c3dcdc7a5
M-27-mukul <pathakmukul29@gmail.com>
2024-12-12 16:12:19 +0530

    This is my first commit

---
+++ app.py
@@ -0,0 +1,126 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+import mysql.connector
+import os
+from dotenv import load_dotenv
+import json
+
+app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
+
+# Database connection
+def get_db_connection():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+    )
+
+# About Page
+@app.route("/")
+def about():
+    return render_template("about.html")
+
+# Login
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if not session.get('user_id'):
+        if request.method == "POST":
+            email = request.form["email"]
+            password = request.form["password"]
+            conn = get_db_connection()
+            cursor = conn.cursor(dictionary=True)
+            cursor.execute("SELECT * FROM users WHERE email = %s AND password = %s", (email, password))
+            user = cursor.fetchone()
+            if user:
+                session["user_id"] = user["id"]
+                session["user_name"] = user["name"]
+                return redirect(url_for("about"))
+            else:
+                flash("Invalid credentials")
+            cursor.close()
+            conn.close()
+        return render_template("login.html")
+    else:
+        return redirect(url_for("about"))
+
+# Register
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if not session.get('user_id'):
+        if request.method == "POST":
+            name = request.form["name"]
+            email = request.form["email"]
+            password = request.form["password"]
+            conn = get_db_connection()
+            cursor = conn.cursor()
+            try:
+                cursor.execute("INSERT INTO users (name, email, password) VALUES (%s, %s, %s)", (name, email, password))
+                conn.commit()
+                return redirect(url_for("login"))
+            except:
+                flash("Email already exists")
+            cursor.close()
+            conn.close()
+        return render_template("register.html")
+    else:
+        return redirect(url_for("about"))
+
+# logout action
+@app.route("/logout")
+def logout():
+    session['user_id'] = None
+    session['user_name'] = None
+    return redirect(url_for("login"))
+
+# Search Page
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    search_query = request.args.get("recipe_name")
+    if search_query:
+        cursor.execute("SELECT id, recipe_name FROM recipes WHERE recipe_name LIKE %s order by id Limit 4", (f"%{search_query}%",))
+    else:
+        cursor.execute(" SELECT id, recipe_name from recipes order by id Limit 4")
+    recipes_items = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return render_template("search recipe.html", recipes=recipes_items)
+
+# Description Page
+@app.route("/description/<int:recipe_id>", methods=["GET", "POST"])
+def describe(recipe_id):
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    query = "SELECT * FROM recipes WHERE id = "+ str(recipe_id)
+    cursor.execute(query)
+    recipe_item = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    if not recipe_item:
+        return redirect(url_for("search"))
+    recipe = {
+        "id" : recipe_item["id"],
+        "name" : recipe_item["recipe_name"],
+        "desc" : recipe_item["recipe_desc"],
+        "procedure" : json.loads(recipe_item["recipe_procedure"]),
+    }
+    return render_template("describe recipe.html", recipe=recipe)
+
+# Contact Page
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+# Profile Page
+@app.route("/profile")
+def profile():
+    if session.get('user_id'):
+        return render_template("profile.html")
+    else:
+        return redirect(url_for("login"))
+
+
+if __name__ =="__main__":
+    app.run(debug=True)
---
+++ db_setup.py
@@ -0,0 +1,63 @@
+import os
+import mysql.connector
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+# Database credentials from environment variables
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+# Connect to MySQL
+conn = mysql.connector.connect(
+    host=DB_HOST,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    database=DB_NAME,
+)
+
+cursor = conn.cursor()
+
+# Create database and tables
+# cursor.execute(f"CREATE DATABASE IF NOT EXISTS {DB_NAME}")
+# conn.database = DB_NAME
+
+# Recipes table
+# cursor.execute("""
+# CREATE TABLE IF NOT EXISTS recipes (
+#     id INT AUTO_INCREMENT PRIMARY KEY,
+#     name VARCHAR(255) NOT NULL,
+#     description TEXT NOT NULL,
+#     ingredients TEXT NOT NULL,
+#     price FLOAT NOT NULL
+# )
+# """)
+
+# # Users table
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(60) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+)
+""")
+
+# # Cart table
+# cursor.execute("""
+# CREATE TABLE IF NOT EXISTS cart (
+#     id INT AUTO_INCREMENT PRIMARY KEY,
+#     user_id INT,
+#     recipe_id INT,
+#     quantity INT DEFAULT 1,
+#     FOREIGN KEY (user_id) REFERENCES users(id),
+#     FOREIGN KEY (recipe_id) REFERENCES recipes(id)
+# )
+# """)
+
+print("Database and tables created successfully.")
+cursor.close()
+conn.close()
